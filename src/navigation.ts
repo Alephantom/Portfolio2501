fc82adef480ae452c539a549df46ceefc569e55d
Anna-Lena <Lenchenx@gmx.net>
2025-01-28 22:54:35 +0100

    Refactor navigation structure and enhance footer links for improved accessibility

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,18 +1,20 @@
 import { getPermalink, 
   // getBlogPermalink, 
-  // getAsset 
+
+  getAsset 
 } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    // {
-    //   text: 'Homes',
-    //   href: getPermalink('/homes/personal'),
+    {
+      text: 'Homes',
+      href: getPermalink('/homes/personal'),
         
       
-    // },
-    // {
-    //   text: 'Pages',
+    },
+    {
+      text: 'About Me',
+          href: getPermalink('/about'),
     //   links: [
     //     {
     //       text: 'Features (Anchor Link)',
@@ -43,7 +45,7 @@ export const headerData = {
     //       href: getPermalink('/privacy'),
     //     },
     //   ],
-    // },
+    },
     // {
     //   text: 'Landing',
     //   links: [
@@ -98,72 +100,72 @@ export const headerData = {
     //     },
     //   ],
     // },
-    // {
-    //   text: 'Widgets',
-    //   href: '#',
-    // },
+    {
+      text: 'Widgets',
+      href: '#',
+    },
   ],
   actions: [{ text: 'Connect', href: '/contact', target: '_blank' }],
 };
 
 export const footerData = {
-  // links: [
-  //   {
-  //     title: 'Product',
-  //     links: [
-  //       { text: 'Features', href: '#' },
-  //       { text: 'Security', href: '#' },
-  //       { text: 'Team', href: '#' },
-  //       { text: 'Enterprise', href: '#' },
-  //       { text: 'Customer stories', href: '#' },
-  //       { text: 'Pricing', href: '#' },
-  //       { text: 'Resources', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Platform',
-  //     links: [
-  //       { text: 'Developer API', href: '#' },
-  //       { text: 'Partners', href: '#' },
-  //       { text: 'Atom', href: '#' },
-  //       { text: 'Electron', href: '#' },
-  //       { text: 'AstroWind Desktop', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Support',
-  //     links: [
-  //       { text: 'Docs', href: '#' },
-  //       { text: 'Community Forum', href: '#' },
-  //       { text: 'Professional Services', href: '#' },
-  //       { text: 'Skills', href: '#' },
-  //       { text: 'Status', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Company',
-  //     links: [
-  //       { text: 'About', href: '#' },
-  //       { text: 'Blog', href: '#' },
-  //       { text: 'Careers', href: '#' },
-  //       { text: 'Press', href: '#' },
-  //       { text: 'Inclusion', href: '#' },
-  //       { text: 'Social Impact', href: '#' },
-  //       { text: 'Shop', href: '#' },
-  //     ],
-  //   },
-  // ],
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Atom', href: '#' },
+        { text: 'Electron', href: '#' },
+        { text: 'AstroWind Desktop', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
+      ],
+    },
+  ],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  // socialLinks: [
-  //   { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-  //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-  //   { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-  //   { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-  //   { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-  // ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+  ],
   footNote: `
     Made by <a class="text-purple-800 underline dark:text-muted" href="https://annalenabirkner.com/"> Alephantom</a> · All rights reserved.
   `,
